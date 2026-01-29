9e3e4032bc69e22860e588e390dc57b1e187a8df
Urios Matthieu <urios.matthieu@gmail.com>
2026-01-29 17:13:48 +0100

    3h de fix d'OCaml tout ça juste pour faire bouger la camera quand le personnage atteint le milieu de l'écran. J'aime ma vie...

--- src/components/player.ml
+++ src/components/player.ml
@@ -1,3 +1,7 @@
+open Ecs
+open Component_defs
+open System_defs
+
 let players () =  
   Block.create Cst.(player1_x, player1_y, Vector.zero, player_color, player_width, player_height, 100.,Some Cst.g, None, 0., 2)
 
@@ -13,8 +17,13 @@ let stop_players () =
 
 let move player v =
   player#velocity#set  (Vector.add v player#velocity#get);
-  Camera.(move v (camera ()));
-  ()
+  let Vector.{x; y} = player#dposition#get in
+  Gfx.debug "x = %f ; y = %f\n%!" x y;
+  Gfx.debug "%f\n%!" (float Cst.window_width);
+  if x >= (float Cst.window_width) /. 2. then
+    Camera.(move v (camera()))
+  else
+    ()
 
 let jump player =
   player#velocity#set (Vector.add Cst.player_v_jump player#velocity#get)
