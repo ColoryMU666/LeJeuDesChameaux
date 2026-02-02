1d79e611d4e22330fd38a7bf7ae351038c04987b
ColoryMU666 <urios.matthieu@gmail.com>
2026-02-02 21:12:51 +0100

    Fixed some bugs
    TODO : Reimplement camera

--- src/components/player.ml
+++ src/components/player.ml
@@ -10,9 +10,9 @@ let resolve (v : Vector.t) (player : player) (reacter : tag) =
 let create (x, y, v, txt, width, height, mass) =
   let e = new player () in
   e#texture#set txt;
-  e#position#set Vector.{x=float x ; y = float y};
+  e#position#set Vector.{ x = float x ; y = float y };
   e#velocity#set v;
-  e#box#set Rect.{width;height};
+  e#box#set Rect.{ width = width ; height = height };
   e#mass#set mass;
   e#forces#set Cst.g;
   e#friction#set Vector.{x = 0.5 ; y = 1.};
@@ -24,7 +24,7 @@ let create (x, y, v, txt, width, height, mass) =
   e
 
 
-let player = create (100, 100, Vector.{x=0. ; y=0.}, Texture.blue, 20, 50, 1.)
+let player = create (Cst.player1_x, Cst.player1_y, Vector.{x=0. ; y=0.}, Texture.blue, 20, 50, 1.)
 
 
 let move_direction d =
