7d68f0879c30314a2636e67818e47db97d477f2b
ColoryMU666 <urios.matthieu@gmail.com>
2026-03-26 14:50:24 +0100

    Fixed shotgun  + added Rockel Launcher
    TODO : Textures

--- src/components/gun.ml
+++ src/components/gun.ml
@@ -5,11 +5,30 @@ open System_defs
 (*
 Glock id = 0
 Laser id = 1
+Shotgun id = 2
+Rocket launcher id = 3
 *)
 
 let resolve (v : Vector.t) (ammo : ammunition) (reacter : tag) =
   ammo#tokill#set true
 
+let resolve_rl (v : Vector.t) (ammo : ammunition) (reacter : tag) =
+  ammo#tokill#set true;
+  let a  = new ammunition () in
+  let values = Block.{ Block.default_set_values with
+    pos_x = ammo#position#get.x -. 50.;
+    pos_y = ammo#position#get.y -. 50.;
+    velocity = Vector.zero;
+    texture = Texture.(purple);
+    width = 110;
+    height = 110;
+    resolve = (fun (v:Vector.t) (reacter:tag) -> ());
+    elasticity = 0.;
+    tag = Ally_projectile_tag {damage = 50.}
+    } in
+  Block.set_block a values;
+  Time.create_timer 500. (fun dt -> a#tokill#set true)
+  
 let interact_resolver (gun : gun) =
   Gfx.debug "interaction avec gun d'id %d\n%!" gun#gun_id#get;
   let Global.{player1; _} = Global.get () in
@@ -29,7 +48,7 @@ let fire_laser () =
   let a = new ammunition () in
   let Global.{main_camera ; player1 ; _} = Global.get () in
   let x = player1#position#get.x +. float(Cst.player_width) /. 2. in
-  let y = player1#position#get.y in
+  let y = player1#position#get.y +. float(Cst.player_height) /. 2. in
   let target_x = ((float !Global.mouse_x) -. x) +. main_camera#position#get.x in
   let target_y = ((float !Global.mouse_y) -. y) +. main_camera#position#get.y in
   let values = Block.{ Block.default_set_values with
@@ -43,14 +62,13 @@ let fire_laser () =
     elasticity = 0.;
     tag = Ally_projectile_tag {damage = 10.}
     } in
-  Block.set_block a values;
-  a
+  Block.set_block a values
 
 let fire_glock () =
   let a = new ammunition () in
   let Global.{main_camera ; player1 ; _} = Global.get () in
   let x = player1#position#get.x +. float(Cst.player_width) /. 2. in
-  let y = player1#position#get.y in
+  let y = player1#position#get.y +. float(Cst.player_height) /. 2. in
   let target_x = ((float !Global.mouse_x) -. x) +. main_camera#position#get.x in
   let target_y = ((float !Global.mouse_y) -. y) +. main_camera#position#get.y in
   let values = Block.{ Block.default_set_values with
@@ -64,10 +82,51 @@ let fire_glock () =
     elasticity = 0.;
     tag = Ally_projectile_tag {damage = 1.}
     } in
-  Block.set_block a values;
-  a
+  Block.set_block a values
 
-let fire_func_ar = [|fire_glock; fire_laser|]
+let fire_shotgun () =
+  let Global.{main_camera ; player1 ; _} = Global.get () in
+  let x = player1#position#get.x +. float(Cst.player_width) /. 2. in
+  let y = player1#position#get.y +. float(Cst.player_height) /. 2. in
+  let target_x = ((float !Global.mouse_x) -. x) +. main_camera#position#get.x in
+  let target_y = ((float !Global.mouse_y) -. y) +. main_camera#position#get.y in
+  for i = -2 to 2 do
+    let a = new ammunition () in
+    let values = Block.{ Block.default_set_values with
+    pos_x = x;
+    pos_y = y;
+    velocity = Vector.(mult 10. (normalize {x = target_x +. ((float i) *. 20.); y = target_y +. ((float i) *. 20.)}));
+    texture = Texture.yellow;
+    width = 10;
+    height = 10;
+    resolve = (fun (v:Vector.t) (reacter:tag) -> resolve v a reacter);
+    elasticity = 0.;
+    tag = Ally_projectile_tag {damage = 25.}
+    } in
+  Block.set_block a values
+  done
+
+let fire_rl () =
+  let a = new ammunition () in
+  let Global.{main_camera ; player1 ; _} = Global.get () in
+  let x = player1#position#get.x +. float(Cst.player_width) /. 2. in
+  let y = player1#position#get.y +. float(Cst.player_height) /. 2. in
+  let target_x = ((float !Global.mouse_x) -. x) +. main_camera#position#get.x in
+  let target_y = ((float !Global.mouse_y) -. y) +. main_camera#position#get.y in
+  let values = Block.{ Block.default_set_values with
+    pos_x = x;
+    pos_y = y;
+    velocity = Vector.(mult 5. (normalize {x = target_x ; y = target_y}));
+    texture = Texture.purple;
+    width = 10;
+    height = 10;
+    resolve = (fun (v:Vector.t) (reacter:tag) -> resolve_rl v a reacter);
+    elasticity = 0.;
+    tag = Ally_projectile_tag {damage = 30.}
+    } in
+  Block.set_block a values
+
+let fire_func_ar = [|fire_glock; fire_laser; fire_shotgun ; fire_rl|]
 
 let handle_fire (gun : gun) =
   if gun#can_shoot#get then begin
@@ -91,7 +150,7 @@ let create_laser () =
     default_forces = Some(Cst.g);
     tag = Gun_tag {gunType = 1};
   };
-  g#shoot#set (fun() -> handle_fire g);
+  g#shoot#set (fun () -> handle_fire g);
   g#gun_id#set 1;
   g#fire_rate#set 1.;
   g#interact_resolver#set (fun () -> interact_resolver g);
@@ -103,17 +162,53 @@ let create_glock () =
   Block.set_block g {Block.default_set_values with
     pos_x = 200.;
     pos_y = 100.;
-    texture = Texture.yellow;
+    texture = Texture.magenta;
     width = 10;
     height = 10;
     default_forces = Some(Cst.g);
     tag = Gun_tag {gunType = 0};
   };
-  g#shoot#set (fun() -> handle_fire g);
+  g#shoot#set (fun () -> handle_fire g);
   g#gun_id#set 0;
   g#fire_rate#set 500.;
   g#interact_resolver#set (fun () -> interact_resolver g);
   Interact_system.(register (g :> t));
   g
 
-let () = ignore (create_laser ())
+let create_shotgun () =
+  let g = new gun () in
+  Block.set_block g {Block.default_set_values with
+    pos_x = 200.;
+    pos_y = 100.;
+    texture = Texture.magenta;
+    width = 10;
+    height = 10;
+    default_forces = Some(Cst.g);
+    tag = Gun_tag {gunType = 2};
+  };
+  g#shoot#set (fun () -> handle_fire g);
+  g#gun_id#set 2;
+  g#fire_rate#set 1000.;
+  g#interact_resolver#set (fun () -> interact_resolver g);
+  Interact_system.(register (g :> t));
+  g
+
+let create_rl () =
+  let g = new gun () in
+  Block.set_block g {Block.default_set_values with
+    pos_x = 200.;
+    pos_y = 100.;
+    texture = Texture.magenta;
+    width = 10;
+    height = 10;
+    default_forces = Some(Cst.g);
+    tag = Gun_tag {gunType = 3};
+  };
+  g#shoot#set (fun () -> handle_fire g);
+  g#gun_id#set 3;
+  g#fire_rate#set 1000.;
+  g#interact_resolver#set (fun () -> interact_resolver g);
+  Interact_system.(register (g :> t));
+  g
+
+let () = ignore (create_rl ())
