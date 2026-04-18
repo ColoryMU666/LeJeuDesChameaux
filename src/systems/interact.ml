d793322e99b98e062de6413bd094bab849c9e3b8
Hector Bonhoure <hector.bonhoure@gmail.com>
2026-04-18 13:28:13 +0200

    fixed crash while interacting on two weapons at the same time

--- src/systems/interact.ml
+++ src/systems/interact.ml
@@ -9,10 +9,13 @@ let update _ el =
   let Global.{player1; _} = Global.get () in
   let playerbox = player1#box#get in
   let playerpos = player1#position#get in
-  Seq.iter (fun (e:t) ->
-    let p, r = Rect.mdiff playerpos playerbox e#position#get e#box#get in
-    if Rect.has_origin p r && !Global.is_interacting then
-      e#interact_resolver#get ()
-    else
-      ()
-    ) el
+  if !Global.is_interacting then begin
+    let e_opt = Seq.find (fun (e:t) ->
+      let p, r = Rect.mdiff playerpos playerbox e#position#get e#box#get in
+      Rect.has_origin p r
+      ) el
+    in
+    match e_opt with
+    | None -> ()
+    | Some e -> e#interact_resolver#get ()
+  end
